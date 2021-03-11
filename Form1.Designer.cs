44646fede8b94e6d4aada098948d134997cbbd4b
Robin Meersman <echteenrobin@gmail.com>
2021-03-11 13:17:57 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿
+namespace funcCalc
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.InputPanel = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.resBox = new System.Windows.Forms.TextBox();
+            this.InputPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // InputPanel
+            // 
+            this.InputPanel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.InputPanel.AutoSize = true;
+            this.InputPanel.BackColor = System.Drawing.Color.Lime;
+            this.InputPanel.Controls.Add(this.button1);
+            this.InputPanel.Controls.Add(this.textBox5);
+            this.InputPanel.Controls.Add(this.textBox4);
+            this.InputPanel.Controls.Add(this.textBox3);
+            this.InputPanel.Controls.Add(this.textBox2);
+            this.InputPanel.Controls.Add(this.textBox1);
+            this.InputPanel.Location = new System.Drawing.Point(0, 0);
+            this.InputPanel.Name = "InputPanel";
+            this.InputPanel.Size = new System.Drawing.Size(805, 100);
+            this.InputPanel.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(717, 27);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 34);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox5.Location = new System.Drawing.Point(640, 34);
+            this.textBox5.MaxLength = 20;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.PlaceholderText = "End value";
+            this.textBox5.Size = new System.Drawing.Size(65, 23);
+            this.textBox5.TabIndex = 4;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox4.Location = new System.Drawing.Point(569, 34);
+            this.textBox4.MaxLength = 20;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.PlaceholderText = "Start value";
+            this.textBox4.Size = new System.Drawing.Size(65, 23);
+            this.textBox4.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.textBox3.Location = new System.Drawing.Point(210, 34);
+            this.textBox3.MaxLength = 20;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.PlaceholderText = "Value for c";
+            this.textBox3.Size = new System.Drawing.Size(93, 23);
+            this.textBox3.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.textBox2.Location = new System.Drawing.Point(111, 34);
+            this.textBox2.MaxLength = 20;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.PlaceholderText = "Value for b";
+            this.textBox2.Size = new System.Drawing.Size(93, 23);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.textBox1.Location = new System.Drawing.Point(12, 34);
+            this.textBox1.MaxLength = 20;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.PlaceholderText = "Value for a";
+            this.textBox1.Size = new System.Drawing.Size(93, 23);
+            this.textBox1.TabIndex = 0;
+            // 
+            // resBox
+            // 
+            this.resBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.resBox.Cursor = System.Windows.Forms.Cursors.Default;
+            this.resBox.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.resBox.Location = new System.Drawing.Point(0, 98);
+            this.resBox.MaxLength = 255000;
+            this.resBox.Multiline = true;
+            this.resBox.Name = "resBox";
+            this.resBox.PlaceholderText = "Results...";
+            this.resBox.ReadOnly = true;
+            this.resBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.resBox.Size = new System.Drawing.Size(801, 352);
+            this.resBox.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.resBox);
+            this.Controls.Add(this.InputPanel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Function Calculator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.InputPanel.ResumeLayout(false);
+            this.InputPanel.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel InputPanel;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox resBox;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace funcCalc
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Width = Screen.PrimaryScreen.WorkingArea.Width / 2;
+            Height = Screen.PrimaryScreen.WorkingArea.Height / 2;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // check if any box is empty...
+                if(textBox1.Text == "" || textBox2.Text == "" || textBox3.Text == "" || textBox4.Text == "" || textBox5.Text == "")
+                {
+                  DialogResult error = MessageBox.Show("Gelieve alles in te vullen", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                } else if (int.Parse(textBox4.Text) > int.Parse(textBox5.Text))
+                {
+                    DialogResult error = MessageBox.Show("De waarde voor start moet kleiner zijn dan de waarde voor einde", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    if(error == DialogResult.OK)
+                    {
+                        textBox4.Text = "";
+                        textBox5.Text = "";
+                    }
+                } else
+                {
+                    int a = int.Parse(textBox1.Text);
+                    int b = int.Parse(textBox2.Text);
+                    int c = int.Parse(textBox3.Text);
+                    int start = int.Parse(textBox4.Text);
+                    int end = int.Parse(textBox5.Text);
+
+                    for (int i = start; i < end + 1; i += 1)
+                    {
+                        int res = a * i * i + b * i + c;
+
+                        resBox.Text += "For x = " + i + ", f(x) = " + res + "\r\n";
+                    }
+                }
+            } catch(Exception err)
+            {
+                DialogResult error = MessageBox.Show(err.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                if(error == DialogResult.OK) { 
+                    textBox1.Text = "";
+                    textBox2.Text = "";
+                    textBox3.Text = "";
+                    textBox4.Text = "";
+                    textBox5.Text = "";
+                }
+            }
+        }
+    }
+}
